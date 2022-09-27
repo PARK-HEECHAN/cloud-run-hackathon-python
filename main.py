653f35117e932850072131deec2bc08dfcc99259
PARK-HEECHAN <andrew6371@naver.com>
2022-09-27 06:21:04 +0000

    final version

--- main.py
+++ main.py
@@ -40,51 +40,49 @@ def move():
     all_state = request.json['arena']['state']
 
 
-    if my_state['x'] == 0 and my_state['y'] == (dims[1] - 1) :
+    if my_state['x'] == (dims[0] - 1) and my_state['y'] == 0 :
         for key, value in all_state.items():
             if key != 'https://cloud-run-hackathon-python-7ei5boopga-uc.a.run.app':
-                if my_state['x'] == value['x'] and my_state['y'] <= (value['y'] + 3):
-                    if my_state['direction'] == 'N':
-                        return moves[1]
-                    elif my_state['direction'] == 'E' or my_state['direction'] == 'S':
-                        return moves[2]
+                if my_state['x'] == value['x'] and my_state['y'] >= (value['y'] - 3):
+                    if my_state['direction'] == 'S':
+                        return 'T'
+                    elif my_state['direction'] == 'E' or my_state['direction'] == 'N':
+                        return 'R'
                     else:
-                        return moves[3]
-                elif my_state['x'] >= (value['x'] - 3) and my_state['y'] == value['y']:
-                    if my_state['direction'] == 'E':
-                        return moves[1]
-                    elif my_state['direction'] == 'N' or my_state['direction'] == 'W':
-                        return moves[3]
+                        return 'L'
+                elif my_state['x'] <= (value['x'] + 3) and my_state['y'] == value['y']:
+                    if my_state['direction'] == 'W':
+                        return 'T'
+                    elif my_state['direction'] == 'N' or my_state['direction'] == 'E':
+                        return 'L'
                     else:
-                        return moves[2]
-            else:
-                continue
+                        return 'R'
     
-    if  my_state['x'] > 0 :
-        if my_state['direction'] == 'W' :
+    if my_state['y'] > 0:
+        if my_state['direction'] == 'N' :
             for key, value in all_state.items():
                 if key != 'https://cloud-run-hackathon-python-7ei5boopga-uc.a.run.app':
-                    if my_state['x'] == (value['x'] + 1) and my_state['y'] == value['y']:
-                        return moves[1]
-            return moves[0]
+                    if my_state['x'] == value['x'] and my_state['y'] <= (value['y'] + 3):
+                        return 'T'
+            return 'F'
         elif my_state['direction'] == 'E' or my_state['direction'] == 'S':
-            return moves[3]
-        elif my_state['direction'] == 'N' :
-            return moves[2]
-    
-    if my_state['y'] < (dims[1] - 1):
-        if my_state['direction'] == 'S' :
+            return 'L'
+        elif my_state['direction'] == 'W' :
+            return 'R'
+
+    if  my_state['x'] < (dims[0] - 1) :
+        if my_state['direction'] == 'E' :
             for key, value in all_state.items():
                 if key != 'https://cloud-run-hackathon-python-7ei5boopga-uc.a.run.app':
-                    if my_state['x'] == value['x'] and my_state['y'] == (value['y'] - 1):
-                        return moves[1]
-            return moves[0]
-        elif my_state['direction'] == 'E' or my_state['direction'] == 'N':
-            return moves[3]
-        elif my_state['direction'] == 'W' :
-            return moves[2]
+                    if my_state['x'] >= (value['x'] - 3) and my_state['y'] == value['y']:
+                        return 'T'
+            return 'F'
+        elif my_state['direction'] == 'W' or my_state['direction'] == 'N':
+            return 'R'
+        elif my_state['direction'] == 'S' :
+            return 'L'
 
-    return moves[1]
+    return 'T'
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
