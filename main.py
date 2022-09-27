b3c52eb95f783277f801eb0e3c289ec90c53261b
PARK-HEECHAN <andrew6371@naver.com>
2022-09-27 04:40:01 +0000

    test

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import logging
 import random
 from flask import Flask, request
 
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+# logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
@@ -31,13 +31,51 @@ def index():
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
-    logger.info(request.json)
+    # logger.info(request.json)
     
     # TODO add your implementation here to replace the random response
     dims = request.json['arena']['dims']
-    print(dims)
     
-    return moves[random.randrange(len(moves))]
+    my_state = request.json['arena']['state']['https://cloud-run-hackathon-python-7ei5boopga-uc.a.run.app']
+    all_state = request.json['arena']['state']
+
+    if my_state['x'] == 0 and my_state['y'] == (dims[1] - 1) :
+        for key, value in all_state.items():
+            if key != 'https://cloud-run-hackathon-python-7ei5boopga-uc.a.run.app':
+                if my_state['x'] == value['x'] and my_state['y'] <= (value['y'] + 3):
+                    if my_state['direction'] == 'N':
+                        return moves[1]
+                    elif my_state['direction'] == 'E' or my_state['direction'] == 'S':
+                        return moves[2]
+                    else:
+                        return moves[3]
+                elif my_state['x'] >= (value['x'] - 3) and my_state['y'] == value['y']:
+                    if my_state['direction'] == 'E':
+                        return moves[1]
+                    elif my_state['direction'] == 'N' or my_state['direction'] == 'W':
+                        return moves[3]
+                    else:
+                        return moves[2]
+            else:
+                continue
+    
+    if  my_state['x'] > 0 :
+        if my_state['direction'] == 'W' :
+            return moves[0]
+        elif my_state['direction'] == 'E' or my_state['direction'] == 'S':
+            return moves[3]
+        elif my_state['direction'] == 'N' :
+            return moves[2]
+    
+    if my_state['y'] < (dims[1] - 1):
+        if my_state['direction'] == 'S' :
+            return moves[0]
+        elif my_state['direction'] == 'E' or my_state['direction'] == 'N':
+            return moves[3]
+        elif my_state['direction'] == 'W' :
+            return moves[2]
+
+    return moves[1]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
